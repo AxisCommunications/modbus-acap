37458228e96f1906f0cccc2adde0330f34a2f6d2
Joakim Roubert <joakimr@axis.com>
2024-10-24 15:28:59 +0200

    Add HTML configuration page
    This helps configuring which scenario to select.
    
    Change-Id: I5bf6e2f4e59b424d2075ede49d7fe976ee970941
    Signed-off-by: Joakim Roubert <joakimr@axis.com>

---
+++ html/js/modbusconfig.js
@@ -0,0 +1,128 @@
+var params = {};
+var scenarios = {};
+const appname = 'Modbusacap';
+const parambaseurl = '/axis-cgi/param.cgi?action=';
+const paramgeturl = parambaseurl + 'list&group= ' + appname + '.';
+const paramseturl = parambaseurl + 'update&' + appname + '.';
+const scenariourl = '/local/objectanalytics/control.cgi';
+const table = document.getElementById("scenarios");
+const port = document.getElementById("port");
+const server = document.getElementById("server");
+const clientcfg = document.getElementById("clientcfg");
+const modeselection = document.getElementById("modeselection");
+const scenarioselection = document.getElementById("scenarioselection");
+const UpdateInterval = 5;
+const SERVER = 0;
+const CLIENT = 1;
+
+function portChange(newport) {
+	setNewValue('Port', newport);
+}
+
+function modeChange(newmode, setparam = true) {
+	if (setparam) {
+		setNewValue('Mode', newmode);
+	}
+	switch (+newmode) {
+		case SERVER:
+			clientcfg.style.display = 'none';
+			break;
+		case CLIENT:
+			clientcfg.style.display = 'block';
+			break;
+		default:
+			console.error('Unknown application mode: ' + newmode);
+	}
+}
+
+function serverChange(newserver) {
+	setNewValue('Server', newserver);
+}
+
+function scenarioChange(newscenario) {
+	setNewValue('Scenario', newscenario);
+}
+
+function updateScenarioTable() {
+	for (let i = table.rows.length - 1; i > 0; i--) {
+		table.deleteRow(i);
+	}
+	for (const scenario of scenarios) {
+		const row = table.insertRow();
+		var i = 0;
+		const nameCell = row.insertCell(i++);
+		const idCell = row.insertCell(i++);
+		const typeCell = row.insertCell(i++);
+
+		idCell.innerText = scenario.id;
+		nameCell.innerText = scenario.name;
+		typeCell.innerText = scenario.type.replace(/([A-Z])/g, ' $1').toLowerCase();
+	}
+}
+
+function updateScenarioSelection() {
+	while (scenarioselection.options.length > 0) {
+		scenarioselection.remove(0);
+	}
+	for (const scenario of scenarios) {
+		scenarioselection.add(new Option(`${scenario.name} (ID: ${scenario.id})`, scenario.id));
+	}
+	scenarioselection.value = +(params['Scenario']);
+}
+
+async function getScenarios() {
+	try {
+		const scenarioData = await $.post({
+			url: scenariourl,
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			data: '{"apiVersion": "1.2", "method": "getConfiguration"}'
+		});
+		scenarios = scenarioData.data.scenarios;
+		for (i in scenarioData.data.scenarios) {
+			console.log('scenario id: ' + scenarios[i].id + ', scenario name: ' + scenarios[i].name + ', type: ' + scenarios[i].type);
+		}
+		updateScenarioTable();
+	} catch (error) {
+		alert('Failed to get scenario info from AOA');
+		console.error('Failed to get scenario info from AOA: ', error)
+	}
+}
+
+async function getCurrentValue(param) {
+	try {
+		const paramData = await $.get(paramgeturl + param);
+		params[param] = paramData.split('=')[1];
+		console.log('Got ' + param + ' value ' + params[param]);
+	} catch (error) {
+		alert('Failed to get parameter ' + param);
+	}
+}
+
+async function setNewValue(param, value) {
+	try {
+		await $.get(paramseturl + param + '=' + value);
+		params[param] = value;
+		console.log('Set ' + param + ' value to ' + value);
+	} catch (error) {
+		alert('Failed to set parameter ' + param);
+	}
+}
+
+async function updateValues() {
+	await getCurrentValue('Mode');
+	await getCurrentValue('Port');
+	await getCurrentValue('Scenario');
+	await getCurrentValue('Server');
+	await getScenarios();
+	modeChange(+(params['Mode']), false);
+	modeselection.value = +(params['Mode']);
+	port.value = +(params['Port']);
+	updateScenarioSelection();
+	server.value = params['Server'];
+	console.log('Will call again in ' + UpdateInterval + ' seconds to make sure we are in sync with device');
+	setTimeout(updateValues, 1000 * UpdateInterval);
+}
+
+updateValues();
